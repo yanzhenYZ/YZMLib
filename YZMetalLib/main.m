5872a148b0266aa55145e0942b88ff854dd004ea
yanzhen <sdyzsdut@163.com>
2020-12-25 19:39:53 +0800

    todo

--- YZMetalLib/main.m
+++ YZMetalLib/main.m
@@ -8,6 +8,16 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+/**
+ 2.0.1 前置后置摄像头
+ 2.0.2 镜像问题
+ 2.0.3 YZMTKView 显示模式问题
+ 
+ 2.0.7 美颜
+ 2.0.8 性能对比 GPU
+ 2.0.9 dealloc
+ */
+
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {

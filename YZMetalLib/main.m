3c4b0200e39a6d310f6617766f1d08e588cc1526
yanzhen <sdyzsdut@163.com>
2021-01-07 17:47:30 +0800

    start 3.0.1

--- YZMetalLib/main.m
+++ YZMetalLib/main.m
@@ -8,6 +8,20 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+/**
+ 
+ 3.0.1 输出分辨率问题
+ 3.0.2 filter协议类封装
+ 3.0.3 Metal 抽出对外工具类
+ 3.0.4 摄像头操作
+ 
+ 3.0.6 打包SDK
+ */
+
+/**iPhone6s 30fps
+ BGRA：渲染没有美颜 55.3s ~~ 9.2%
+ BGRA：渲染美颜    1.43m ~~ 14.3%
+ */
 
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;

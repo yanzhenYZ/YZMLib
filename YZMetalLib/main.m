1b04e738009a135d606bb8a52a1f95234df8073b
yanzhen <sdyzsdut@163.com>
2021-01-09 10:05:26 +0800

    to VideoMetalKit

--- YZMetalLib/main.m
+++ YZMetalLib/main.m
@@ -8,16 +8,6 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
-/**
- 3.0.8 打包SDK
- 3.0.9 Metal 抽出对外工具类
- */
-
-/**iPhone6s 30fps
- BGRA：渲染没有美颜 55.3s ~~ 9.2%
- BGRA：渲染美颜    1.43m ~~ 14.3%
- */
-
 int main(int argc, char * argv[]) {
     NSString * appDelegateClassName;
     @autoreleasepool {
